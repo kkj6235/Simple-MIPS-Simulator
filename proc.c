69699fa1a8315227dea4c5f3ac714b01b2008988
Jeongseob Ahn <jsahn@ajou.ac.kr>
2022-11-02 01:37:12 +0000

    first commit

---
+++ proc.c
@@ -0,0 +1,135 @@
+/***************************************************************/
+/*                                                             */
+/*   MIPS-32 Instruction Level Simulator                       */
+/*                                                             */
+/*   SCE212 Ajou University                                    */
+/*   proc.c                                                    */
+/*                                                             */
+/***************************************************************/
+
+#include <stdio.h>
+#include <malloc.h>
+
+#include "proc.h"
+#include "mem.h"
+#include "util.h"
+
+/***************************************************************/
+/* System (CPU and Memory) info.                                             */
+/***************************************************************/
+struct MIPS32_proc_t g_processor;
+
+
+/***************************************************************/
+/* Fetch an instruction indicated by PC                        */
+/***************************************************************/
+int fetch(uint32_t pc)
+{
+    return mem_read_32(pc);
+}
+
+/***************************************************************/
+/* TODO: Decode the given encoded 32bit data (word)            */
+/***************************************************************/
+struct inst_t decode(int word)
+{
+    struct inst_t inst;
+    
+    return inst;
+}
+
+/***************************************************************/
+/* TODO: Execute the decoded instruction                       */
+/***************************************************************/
+void execute(struct inst_t inst)
+{
+
+}
+
+/***************************************************************/
+/* Advance a cycle                                             */
+/***************************************************************/
+void cycle()
+{
+    int inst_reg;
+    struct inst_t inst;
+
+    // 1. fetch
+    inst_reg = fetch(g_processor.pc);
+    g_processor.pc += BYTES_PER_WORD;
+
+    // 2. decode
+    inst = decode(inst_reg);
+
+    // 3. execute
+    execute(inst);
+
+    // 4. update stats
+    g_processor.num_insts++;
+    g_processor.ticks++;
+}
+
+/***************************************************************/
+/*                                                             */
+/* Procedure : rdump                                           */
+/*                                                             */
+/* Purpose   : Dump current register and bus values to the     */
+/*             output file.                                    */
+/*                                                             */
+/***************************************************************/
+void rdump() {
+    int k;
+
+    printf("\n[INFO] Current register values :\n");
+    printf("-------------------------------------\n");
+    printf("PC: 0x%08x\n", g_processor.pc);
+    printf("Registers:\n");
+    for (k = 0; k < MIPS_REGS; k++)
+        printf("R%d: 0x%08x\n", k, g_processor.regs[k]);
+}
+
+
+
+/***************************************************************/
+/*                                                             */
+/* Procedure : run n                                           */
+/*                                                             */
+/* Purpose   : Simulate MIPS for n cycles                      */
+/*                                                             */
+/***************************************************************/
+void run(int num_cycles) {
+    int i;
+
+    if (g_processor.running == FALSE) {
+        printf("[ERROR] Can't simulate, Simulator is halted\n\n");
+        return;
+    }
+
+    printf("[INFO] Simulating for %d cycles...\n", num_cycles);
+    for (i = 0; i < num_cycles; i++) {
+        if (g_processor.running == FALSE) {
+            printf("[INFO] Simulator halted\n");
+            break;
+        }
+        cycle();
+    }
+}
+
+/***************************************************************/
+/*                                                             */
+/* Procedure : go                                              */
+/*                                                             */
+/* Purpose   : Simulate MIPS until HALTed                      */
+/*                                                             */
+/***************************************************************/
+void go() {
+    if (g_processor.running == FALSE) {
+        printf("[ERROR] Can't simulate, Simulator is halted\n\n");
+        return;
+    }
+
+    printf("[INFO] Simulating...\n");
+    while (g_processor.running)
+        cycle();
+    printf("[INFO] Simulator halted\n");
+}
